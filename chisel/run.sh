9f63770fd8501e2a95c0eb118220cb11ed0900f2
Sergio Oller <sergioller@gmail.com>
2020-01-29 05:28:41 +0100

    Fix ha host and port

--- chisel/run.sh
+++ chisel/run.sh
@@ -4,8 +4,6 @@ set +u
 CONFIG_PATH=/data/options.json
 SYSTEM_USER=/data/system_user.json
 
-HA_HOST="$(jq --raw-output '.ha_host' $CONFIG_PATH)"
-HA_PORT="$(jq --raw-output '.ha_port' $CONFIG_PATH)"
 CHISEL_SERVER="$(jq --raw-output '.chisel_server' $CONFIG_PATH)"
 CHISEL_R_HOST="$(jq --raw-output '.chisel_remote_host' $CONFIG_PATH)"
 CHISEL_R_PORT="$(jq --raw-output '.chisel_remote_port' $CONFIG_PATH)"
@@ -15,12 +13,10 @@ CHISEL_PASSWORD="$(jq --raw-output '.chisel_password' $CONFIG_PATH)"
 KEEPALIVE="$(jq --raw-output '.keepalive' $CONFIG_PATH)"
 MAX_RETRY_COUNT="$(jq --raw-output '.max_retry_count' $CONFIG_PATH)"
 
-# FIXME: try to set  hahost to homeassistant and haport to 8123 and then drop host network.
-
 exec /usr/local/bin/chisel client \
    --fingerprint "${FINGERPRINT}" \
    --auth "${CHISEL_USER}:${CHISEL_PASSWORD}" \
    --keepalive "${KEEPALIVE}" \
    --max-retry-count "${MAX_RETRY_COUNT}" \
    "${CHISEL_SERVER}" \
-       "R:${CHISEL_R_HOST}:${CHISEL_R_PORT}:${HA_HOST}:${HA_PORT}"
+       "R:${CHISEL_R_HOST}:${CHISEL_R_PORT}:homeassistant:8123"
