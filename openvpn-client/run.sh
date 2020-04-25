a4ce63211dc8caa53f1200d8ff55fdf59e306a45
Sergio Oller <sergioller@gmail.com>
2020-04-25 22:25:26 +0200

    List of strings

--- openvpn-client/run.sh
+++ openvpn-client/run.sh
@@ -3,10 +3,10 @@ if [ ! -c /dev/net/tun ]; then
     mknod /dev/net/tun c 10 200
 fi
 
-jq --raw-output '.config' /data/options.json > /openvpn.conf
+jq --raw-output '".config" | join("\n")' /data/options.json > /openvpn.conf
 jq --raw-output '.key' /data/options.json > /openvpn.key
 
-	jq --raw-output '.user' /data/options.json > /user_pass.conf
+jq --raw-output '.user' /data/options.json > /user_pass.conf
 jq --raw-output '.password' /data/options.json >> /user_pass.conf
 
 openvpn --secret /openvpn.key --config /openvpn.conf
