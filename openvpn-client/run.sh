c0a6223d7383094c754571d7469dffa473e74335
Sergio Oller <sergioller@gmail.com>
2020-04-26 09:12:54 +0200

    bug fixes

--- openvpn-client/run.sh
+++ openvpn-client/run.sh
@@ -4,13 +4,13 @@ if [ ! -c /dev/net/tun ]; then
     mknod /dev/net/tun c 10 200
 fi
 
-jq --raw-output '".config" | join("\n")' /data/options.json > /openvpn.conf
+jq --raw-output '.config[] | join("\n")' /data/options.json > /openvpn.conf
 jq --raw-output '.key' /data/options.json > /openvpn.key
 
 jq --raw-output '.user' /data/options.json > /user_pass.conf
 jq --raw-output '.password' /data/options.json >> /user_pass.conf
 
-key_size=$(wc --bytes < /openvpn.key)
+key_size=$(wc -c < /openvpn.key)
 secret_cmd=
 if [ $key_size -ge 2 ]; then
   secret_cmd="--secret /openvpn.key"
