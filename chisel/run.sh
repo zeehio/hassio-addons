e3be03aab9e317b80611845a3c6902f4ac263d8c
Sergio Oller <sergioller@gmail.com>
2020-12-20 14:17:54 +0100

    Use bashio helpers

--- chisel/run.sh
+++ chisel/run.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bashio
 set +u
 
+
 CONFIG_PATH=/data/options.json
 SYSTEM_USER=/data/system_user.json
 
-CHISEL_SERVER="$(jq --raw-output '.chisel_server' $CONFIG_PATH)"
-CHISEL_R_HOST="$(jq --raw-output '.chisel_remote_host' $CONFIG_PATH)"
-CHISEL_R_PORT="$(jq --raw-output '.chisel_remote_port' $CONFIG_PATH)"
-FINGERPRINT="$(jq --raw-output '.fingerprint' $CONFIG_PATH)"
-CHISEL_USER="$(jq --raw-output '.chisel_user' $CONFIG_PATH)"
-CHISEL_PASSWORD="$(jq --raw-output '.chisel_password' $CONFIG_PATH)"
-KEEPALIVE="$(jq --raw-output '.keepalive' $CONFIG_PATH)"
-MAX_RETRY_COUNT="$(jq --raw-output '.max_retry_count' $CONFIG_PATH)"
+CHISEL_SERVER=$(bashio::config 'chisel_server')
+CHISEL_R_HOST=$(bashio::config 'chisel_remote_host')
+CHISEL_R_PORT=$(bashio::config 'chisel_remote_port')
+FINGERPRINT=$(bashio::config 'fingerprint')
+CHISEL_USER=$(bashio::config 'chisel_user')
+CHISEL_PASSWORD=$(bashio::config 'chisel_password')
+KEEPALIVE=$(bashio::config 'keepalive')
+MAX_RETRY_COUNT=$(bashio::config 'max_retry_count')
 
 exec /usr/local/bin/chisel client \
    --fingerprint "${FINGERPRINT}" \
