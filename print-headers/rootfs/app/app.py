4b7e27dda7a7d7e7d7d7d0bcd663dd910310921f
Sergio Oller <sergioller@gmail.com>
2020-12-28 19:04:00 +0100

    Test

--- print-headers/rootfs/app/app.py
+++ print-headers/rootfs/app/app.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from flask import Flask, request, jsonify, make_response
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
@@ -8,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    print("Hello world")
+    print("Hello world", file=sys.stderr)
     response = make_response(
         jsonify(
             dict(request.headers)
@@ -18,11 +19,11 @@ def hello_world():
     response.headers["Content-Type"] = "application/json"
     return response
 
-
 if __name__ == '__main__':
-    subdir = os.getenv("HOSTNAME").replace("-", "_")  # FIXME: Not sure if that is reliable enough...
+    base_url = "/" + os.getenv("HOSTNAME").replace("-", "_")  # FIXME: Not sure if that is reliable enough...
+    print(base_url, file=sys.stderr)
     application = DispatcherMiddleware(app, {
-        '/' + subdir + "/": app
+        base_url: app
     })
 
     run_simple(hostname='0.0.0.0', port=8099, application=application, use_reloader=True)
