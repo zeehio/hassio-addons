3ab7569ba070ceccdc78afc039215d574ef9ebc0
Sergio Oller <sergioller@gmail.com>
2020-12-28 19:23:04 +0100

    We should be there

--- print-headers/rootfs/app/app.py
+++ print-headers/rootfs/app/app.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    print("Hello world", file=sys.stderr)
     response = make_response(
         jsonify(
             dict(request.headers)
@@ -20,11 +19,9 @@ def hello_world():
     return response
 
 if __name__ == '__main__':
-    base_url = "/" + os.getenv("HOSTNAME").replace("-", "_")  # FIXME: Not sure if that is reliable enough...
-    print(base_url, file=sys.stderr)
+    base_url = os.getenv("INGRESS_ENTRYPOINT")
     application = DispatcherMiddleware(app, {
         base_url: app
     })
-
     run_simple(hostname='0.0.0.0', port=8099, application=application, use_reloader=True)
 
--- print-headers/rootfs/run.sh
+++ print-headers/rootfs/run.sh
@@ -3,7 +3,4 @@
 cd /app
 
 export INGRESS_ENTRYPOINT=$(bashio::addon.ingress_entry)
-export INGRESS_URL=$(bashio::addon.ingress_url)
-
-env
 exec /usr/bin/python3 ./app.py
