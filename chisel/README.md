1088945459e05dea493518bbbb18731ccac7e993
Sergio Oller <sergioller@gmail.com>
2022-06-13 22:15:27 +0200

    Remove chisel

--- chisel/README.md
+++
@@ -1,2 +0,0 @@
-- Work in progress
-- No time to support this
--- chisel/server/rootfs/usr/local/bin/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-envsubst '$PORT' < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf || exit 1
-
-exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
-
